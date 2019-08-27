43869f7b94efcb48dc1200da4f28ae06106b3f3a
Lucas Yang <lucas.yang@thomsonreuters.com>
2019-08-27 12:06:12 -0400

    Child Routes:
    Step1: Configuring Child Routes

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -8,6 +8,8 @@ import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { resolve } from 'q';
 import { ProductResolver } from './product-resolver.service';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 @NgModule({
   imports: [
@@ -22,14 +24,31 @@ import { ProductResolver } from './product-resolver.service';
       { 
         path: 'products/:id/edit', 
         component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver }
+        resolve: { resolvedData: ProductResolver },
+        children:[
+          {
+            path: '',
+            redirectTo: 'info',
+            pathMatch: 'full'
+          },
+          {
+            path: 'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
+          }
+        ]
       }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
